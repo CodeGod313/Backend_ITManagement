816acf0d8d2168f7f78432c486d0dfeb3b7ac376
CodeGod313 <codegod313@gmail.com>
2021-05-16 21:33:38 +0300

    Created company

---
+++ src/main/java/by/vita02/backend/company/Company.java
@@ -0,0 +1,19 @@
+package by.vita02.backend.company;
+
+public class Company {
+    private String name;
+    private String country;
+    private String city;
+    private String sphere;
+    private String street;
+    private String apartments;
+
+    public Company(String name, String country, String city, String sphere, String street, String apartments) {
+        this.name = name;
+        this.country = country;
+        this.city = city;
+        this.sphere = sphere;
+        this.street = street;
+        this.apartments = apartments;
+    }
+}
