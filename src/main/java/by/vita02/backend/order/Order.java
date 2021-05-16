9ab8bc0ab84313805714e460b398d5b5673812ce
CodeGod313 <codegod313@gmail.com>
2021-05-17 00:09:13 +0300

    Order now contain ITProject

--- src/main/java/by/vita02/backend/order/Order.java
+++ src/main/java/by/vita02/backend/order/Order.java
@@ -1,5 +1,7 @@
 package by.vita02.backend.order;
 
+import by.vita02.backend.result.ITProject;
+
 import java.util.Date;
 
 public class Order {
@@ -7,6 +9,8 @@ public class Order {
     private Date date;
     private int cost;
     private int  count;
+    private ITProject itProject = null;
+
 
     public Order(boolean isAccepted, int cost, int count) {
         this.isAccepted = isAccepted;
